cb195fb17ac4c10d1cb752c9bcb476dea6070736
Ashutosh <ashu.cse.sln@gmail.com>
2022-10-09 12:46:30 +0530

    UC3 completed...

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 As a User need to
 enter
-a valid Last
-Name - Last name starts with Cap and has
-minimum 3 characters
+a valid email
+- E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl
+& co) and 2 optional (xyz & in) with
+precise @ and . positions
--- UserRegistration.java
+++ UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String regix = "^[A-Z]{1}[a-z]{2,}";
         System.out.println("Enter First Name...");
@@ -27,6 +27,19 @@ public class UserRegistration {
             System.out.println("Your Last Name Accepted");
         } else {
             System.out.println("Invalid Last Name Format...");
+            return;
+        }
+        System.out.println("*********************************");
+        String Eregix = "^[a-zA-Z0-9[_.+-]?]+@[a-zA-Z0-9]+\\.+[a-zA-Z0-9]+(\\.+[a-zA-Z]{2})?";
+        Pattern Epattern = Pattern.compile(Eregix);
+        System.out.println("Enter your Email ID...");
+        String email = sc.next();
+        Matcher Ematcher = Epattern.matcher(email);
+        if (Ematcher.matches()) {
+            System.out.println("Your Email Accepted...");
+        } else {
+            System.out.println("Your Entered Worng Email Address...");
+            return;
         }
     }
 }
